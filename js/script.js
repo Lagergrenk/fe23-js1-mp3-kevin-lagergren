52a6dc1c5caea69978204487b3be4de703b2aa23
Kevin Lagergren <113167092+Lagergrenk@users.noreply.github.com>
2023-12-13 11:16:18 +0100

    Update v.0.4

--- js/script.js
+++ js/script.js
@@ -5,37 +5,70 @@ const form = document.querySelector("form");
 const errorContainer = document.querySelector(".error-container");
 const countryContainer = document.querySelector(".country-container");
 
+// ------------------ API FUNCTIONS ------------------
 //Get country information from API
 //Changin URL depending on search type (name or language)
 async function getCountry(searchType, userInput) {
-  //Changing user input to lowercase and removing spaces
-  let userInputLowerCaseAndNoSpaces = userInput.toLowerCase().trim();
+  //Changing user input to lowercase and replacing spaces with %20
+  let userInputLowerCase = encodeURIComponent(userInput.toLowerCase().trim());
+  console.log("getCountry", searchType, userInputLowerCase);
   let url;
   //Checking search type and changing url accordingly(name of country or language spoken)
   switch (searchType) {
     case "name":
-      url = `https://restcountries.com/v3.1/name/${userInputLowerCaseAndNoSpaces}`;
+      url = `https://restcountries.com/v3.1/name/${userInputLowerCase}`;
       break;
     case "language":
-      url = `https://restcountries.com/v3.1/lang/${userInputLowerCaseAndNoSpaces}`;
+      url = `https://restcountries.com/v3.1/lang/${userInputLowerCase}`;
       break;
     default:
-      throw new Error("Invalid search type");
+      displayError("Invalid input");
+      return null;
   }
   try {
     let response = await fetch(url);
-
     if (!response.ok) {
-      displayError();
-      throw new Error("Something went wrong");
+      if (response.status === 404) {
+        displayError("404");
+      } else {
+        displayError();
+      }
+      return null;
     }
     let data = await response.json();
     return data;
   } catch (error) {
     console.log(error);
+    displayError();
+    return null;
   }
 }
 
+//Validate user input and fetch data from API
+//Display error if input is invalid
+async function validateAndFetchCountries(searchType) {
+  let userInput = getUserInput();
+  //inputvalidation
+  if (!inputValidation(userInput)) {
+    displayError("Invalid input");
+    return null;
+  }
+  let result = await getCountry(searchType, userInput);
+  return result;
+}
+
+//Search for country when user clicks submit button
+//Prepare search, validate input and fetch data from API
+async function searchCountry(event) {
+  event.preventDefault();
+  let searchType = await determineSearchType();
+  let countries = await validateAndFetchCountries(searchType);
+  if (countries) {
+    processCountries(countries);
+  }
+}
+
+// ------------------ DISPLAY FUNCTIONS ------------------
 //Show country information on the page
 function displayCountries(countries) {
   countryContainer.innerHTML = "";
@@ -70,54 +103,60 @@ function displayCountries(countries) {
   });
 }
 
-//Determine if the user picked radio button for name or language
+// ------------------ HELPER FUNCTIONS ------------------
+//Determine wich radio button the user picked for name or language
 function determineSearchType() {
   let searchType = document.querySelector("input[name=option]:checked").value;
   return searchType;
 }
 
-//Get the user input and check if it is empty
+//Get the user input from the form
 function getUserInput() {
   let userInput = document.querySelector("input[name=search]").value;
-  if (userInput === "") {
-    displayError();
+  return userInput;
+}
+
+//inputvalidation for user input
+function inputValidation(input) {
+  //regex for only letters
+  let regex = /^[a-zA-Z]+(\s[a-zA-Z]+)*$/;
+  if (input.match(regex)) {
+    return true;
   } else {
-    return userInput;
+    return false;
   }
 }
 
-// Display error on the page
-function displayError(error) {
-  countryContainer.innerHTML = "";
-  let errorEl = document.createElement("h2");
-  if (error === "404") {
-    errorEl.innerText = "404 Error";
-  } else {
-    errorEl.innerText = "Failed, try again later";
-  }
-  errorContainer.appendChild(errorEl);
-}
-
-//Get the user input and search type and display the country information
-async function searchCountry(event) {
-  errorContainer.innerHTML = "";
-  // Prevent page reload
-  event.preventDefault();
-  //Getting searchtype by name or language
-  let searchType = determineSearchType();
-  //Get user input / displays error if null or empty
-  let userInput = getUserInput();
-  let countries;
-  countries = await getCountry(searchType, userInput);
-
-  //check if countries is empty if not : display countries
-  if (countries.length > 0) {
-    //sort countries by population descending(large first)
+//Process countries and display them on the page
+//Sort countries by population
+function processCountries(countries) {
+  console.log("processCountries", countries);
+  if (countries && countries.length > 0) {
     countries.sort((a, b) => b.population - a.population);
     displayCountries(countries);
-  } else {
-    displayError();
   }
+}
+
+// ------------------ ERROR FUNCTIONS ------------------
+// Display error on the page depending on error code
+function displayError(errorCode) {
+  console.log("displayError", errorCode);
+  // Clearing containers
+  countryContainer.innerHTML = "";
+  errorContainer.innerHTML = "";
+  let errorEl = document.createElement("h2");
+  switch (errorCode) {
+    case "404":
+      errorEl.innerText = "Country not found. Please try a different search.";
+      break;
+    case "Invalid input":
+      errorEl.innerText = "Invalid input.";
+      break;
+    default:
+      errorEl.innerText = "Network error. Please try again later.";
+      break;
+  }
+  errorContainer.appendChild(errorEl);
 }
 
 // Event listener for submit button in the form
